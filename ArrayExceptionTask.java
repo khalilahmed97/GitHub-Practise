f90b7b10b8a27db10f16803ed2398803ae4c9862
Khalil Ahmed Sharif <101020879+khalilahmed97@users.noreply.github.com>
2023-01-18 02:54:53 +0500

    Update ArrayExceptionTask.java

--- ArrayExceptionTask.java
+++ ArrayExceptionTask.java
@@ -2,7 +2,7 @@ package task1;
 import java.util.Scanner;
 public class ArrayExceptionTask {
 	public static void main(String[] args) {
-		int [] numbers = {1, 25, 14, 15, 90, 80, 79, 11, 70, 55};
+		int [] numbers = {11, 250, 104, 15, 90, 80, 79, 11, 70, 55};
 		
 		System.out.print("ENTER THE INDEX OF AN ARRAY: ");
 		Scanner scan = new Scanner(System.in);
